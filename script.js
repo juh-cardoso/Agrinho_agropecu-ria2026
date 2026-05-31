088a29ebd9010f28280c6704a640026dd5e0c5ff
juh-cardoso <julya.cardoso@escola.pr.gov.br>
2026-05-31 12:10:02 -0300

    juh

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log("Agrinho 2025 carregado!");
+
+const botao = document.querySelector(".btn");
+
+botao.addEventListener("click", () => {
+
+    alert(
+        "Obrigado por participar! A mudança começa por você."
+    );
+
+});
